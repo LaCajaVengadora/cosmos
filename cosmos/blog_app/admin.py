248d8d9e32a5ff6a3f85985d81ca46198e30bf2b
LaCajaVengadora_Local <oviedo.cerdeira.lorenzo@gmail.com>
2024-11-14 17:15:41 -0300

    Mod svg & admin models view (para Valen)

--- cosmos/blog_app/admin.py
+++ cosmos/blog_app/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 from blog_app.models import *
 # Register your models here.
 
-admin.site.register(Comment); admin.site.register(Topic); admin.site.register(Post)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'date', 'author']
+    search_fields = ['author', 'id']
+    #list_filter = ['aut']
+    #readonly_fields = ['date']
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'display_topic', 'date', 'author']
+    search_fields = ['id', 'author__username', 'title', ]
+    list_filter = ['topic', 'date']
+    def display_topic(self, obj): return " - ".join([t.name for t in obj.topic.all()])
+    display_topic.short_description = 'Topics'
+    #readonly_fields = ['date']
+
+
+admin.site.register(Comment, CommentAdmin); admin.site.register(Topic); admin.site.register(Post, PostAdmin)
--- cosmos/uni_app/admin.py
+++ cosmos/uni_app/admin.py
@@ -2,7 +2,33 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Cat); admin.site.register(Uni)
-admin.site.register(Carrera); admin.site.register(Curso); admin.site.register(Beca)
+
+class UniAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'tipo', 'location']
+    search_fields = ['name', 'id', 'location']
+    list_filter = ['tipo']
+    #readonly_fields = ['date']
+class Template(admin.ModelAdmin):
+    def display_uni(self, obj): return obj.uni.name
+    display_uni.short_description = 'Uni'
+    def display_cat(self, obj): return obj.cat.name
+    display_cat.short_description = 'Cat'
+class CarreraAdmin(Template):
+    list_display = ['id', 'name', 'display_uni', 'display_cat', 'precio', 'modalidad', 'duration']
+    search_fields = ['id', 'name' ,'uni__name', 'cat__name']
+    list_filter = ['cat__name', 'modalidad', 'uni__name']
+    #readonly_fields = ['date']
+class CursoAdmin(Template):
+    list_display = ['id', 'name', 'display_uni', 'display_cat', 'precio', 'modalidad', 'duration']
+    search_fields = ['id', 'name' ,'uni__name', 'cat__name']
+    list_filter = ['cat__name', 'modalidad', 'uni__name']
+class BecaAdmin(Template):
+    list_display = ['id', 'name', 'display_uni', 'precio']
+    search_fields = ['id', 'name' ,'uni__name']
+    list_filter = ['uni__name']
+
+
+admin.site.register(Cat); admin.site.register(Uni, UniAdmin)
+admin.site.register(Carrera, CarreraAdmin); admin.site.register(Curso, CursoAdmin); admin.site.register(Beca, BecaAdmin)
 admin.site.register(UniImg); admin.site.register(CursoImg);
 # ALLOWS ADMIN TO SEE models ON ADMIN PANEL
