3bdead04c6e9934160ee89037e35be50cb3fcc01
LaCajaVengadora <127139455+LaCajaVengadora@users.noreply.github.com>
2024-11-26 18:08:25 -0300

    Update cosmos/urls

--- cosmos/cosmos/urls.py
+++ cosmos/cosmos/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.conf.urls import handler404, handler500, handler403, handler400
 from django.conf import settings
 from cosmos.extra import *
 
